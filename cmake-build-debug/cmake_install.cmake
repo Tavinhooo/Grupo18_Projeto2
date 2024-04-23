87451ba101ab04a93344fcc67c81dc822706c8da
Leo00010 <leogrodrigues@gmail.com>
2024-04-23 11:52:09 +0100

    atualizado

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/octav/CLionProjects/Grupo18_Projeto2
+# Install script for directory: C:/Users/Leand/CLionProjects/Grupo18_Projeto2
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/octav/CLionProjects/Grupo18_Projeto2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Leand/CLionProjects/Grupo18_Projeto2/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
