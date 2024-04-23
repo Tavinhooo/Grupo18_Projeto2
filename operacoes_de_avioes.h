ce41b7857d3109579f8f67ec12a6c5ff1f391d7d
Tavinhooo <up202206806@up.pt>
2024-04-23 13:26:44 +0100

    first

---
+++ operacoes_de_avioes.h
@@ -0,0 +1,14 @@
+//
+// Created by octav on 23/04/2024.
+//
+
+#ifndef GRUPO18_PROJETO2_OPERACOES_DE_AVIOES_H
+#define GRUPO18_PROJETO2_OPERACOES_DE_AVIOES_H
+
+
+class operacoes_de_avioes {
+
+};
+
+
+#endif //GRUPO18_PROJETO2_OPERACOES_DE_AVIOES_H
