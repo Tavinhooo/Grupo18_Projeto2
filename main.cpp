ac604c259b6750ae2b0bc13d2f716e9e0de878a4
flashpt931 <162701367+flashpt931@users.noreply.github.com>
2024-04-25 18:51:02 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,40 +2,30 @@
 #include "operacoes_de_avioes.h"
 #include "Abertura_de_ficheiro.h"
 #include "structs.h"
+#include "menu.h"
 
 int main() {
+    srand(time(0));
     const int NUM_AVIOES = 10;
     aviao listaDeAvioes[NUM_AVIOES];
-
-    for (int i = 0; i < NUM_AVIOES; ++i) {
-        listaDeAvioes[i] = criar_aviao();
+    Fila chegando;
+    Nova(chegando);
+    for (int i = 0; i < 10; ++i) {
+        Entra(chegando, criar_aviao());
     }
-    for (int i = 0; i < NUM_AVIOES; ++i) {
-        cout << listaDeAvioes[i].origem << endl;
+    Escreve(chegando);
+    Fila pista;
+    Nova(pista);
+    Fila partida;
+    Nova(partida);
+    while (true) {
+        menu(chegando, pista, partida);
+        cout<<"chegando"<<endl;
+        Escreve(chegando);
+        cout<<"pista"<<endl;
+        Escreve(pista);
+        cout<<"partida"<<endl;
+        Escreve(partida);
     }
-    const int NUM_passageiros = 10;
-    string bilhetes_existentes[NUM_passageiros] = {""}; // Array para armazenar os bilhetes gerados
-    int tamanho_bilhetes = 0; // Variável para acompanhar o tamanho da lista de bilhetes
-
-    passageiro lista_de_passageiros[NUM_passageiros];
-
-    // Define a semente para a geração de números aleatórios
-    srand(time(nullptr));
-
-    // Cria os passageiros
-    for (int i = 0; i < NUM_passageiros; ++i) {
-        lista_de_passageiros[i] = criar_passageiro(bilhetes_existentes, tamanho_bilhetes);
-    }
-
-    // Imprime os nomes dos passageiros
-    for (int i = 0; i < NUM_passageiros; ++i) {
-        cout << "Passageiro " << i + 1 << ":" << endl;
-        cout << "Número do Bilhete: " << lista_de_passageiros[i].numero_bilhete << endl;
-        cout << "Primeiro Nome: " << lista_de_passageiros[i].p_Nome << endl;
-        cout << "Segundo Nome: " << lista_de_passageiros[i].s_Nome << endl;
-        cout << "Nacionalidade: " << lista_de_passageiros[i].nacionalidade << endl;
-        cout << endl;
-    }
-
     return 0;
 }
