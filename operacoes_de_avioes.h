26b2318e81b72f326ae5e2c84990539bd8362623
Tavinhooo <131009513+Tavinhooo@users.noreply.github.com>
2024-04-25 15:36:00 +0100

    Update operacoes_de_avioes.h

--- operacoes_de_avioes.h
+++ operacoes_de_avioes.h
@@ -10,5 +10,10 @@ bool bilhete_existe(string *bilhete_ja_saidos, string &bilhete);
 string criar_bilhete(string *bilhete_ja_saidos, int &tamanho);
 passageiro criar_passageiro(string bilhete_ja_saidos[], int &tamanho);
 aviao criar_aviao();
+bool Vazia(Fila& f);
+void Nova(Fila& f);
+void Entra(Fila& f, aviao data_aviao);
+void Escreve(Fila& f);
+void troca_de_Filas(Fila& f1, Fila &f2);
 
 #endif //GRUPO18_PROJETO2_OPERACOES_DE_AVIOES_H
