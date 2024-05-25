0906c0d96a4444aac6e20c0c8b1befbc57c1a28d
Goncalogama <162703690+Goncalogama@users.noreply.github.com>
2024-05-26 00:15:22 +0100

    Update menu.h

--- menu.h
+++ menu.h
@@ -3,9 +3,6 @@
 #include "structs.h"
 
 
-extern int ciclos_fechado;
-extern bool fechado;
-extern bool aeroporto_abriu;
 int menu(Fila &chegando, Fila &pista, Fila &partida,int argc,char *argv[],string bilhete_ja_saidos[], int &tamanho);
 
 
